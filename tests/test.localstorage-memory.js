0fb06825eb5c67230c6ab6e5b9035edf525b7a4e
Gregor Martynus <gregor@martynus.net>
2015-12-13 10:59:34 -0500

    test: localStorageMemory.length

--- tests/test.localstorage-memory.js
+++ tests/test.localstorage-memory.js
@@ -60,4 +60,16 @@ describe('localStorageMemory()', function () {
       expect(localStorageMemory.getItem(456)).to.be(null);
     });
   });
+  describe('localStorageMemory.length', function () {
+    it ('duplicate puts don\'t increase length', function () {
+      localStorageMemory.setItem('123', 'foo');
+      localStorageMemory.setItem('123', 'foo');
+      expect(localStorageMemory.length).to.be(1);
+    });
+    it ('remove non-existent item doesn\'t decrease length', function () {
+      localStorageMemory.setItem('123', 'foo');
+      localStorageMemory.removeItem('abc');
+      expect(localStorageMemory.length).to.be(1);
+    });
+  });
 });
